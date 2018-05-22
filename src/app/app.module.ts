38de75111a5af8c2bc0152f86dec05f75ed47941
simarpal <simerpal.sangha@gmail.com>
2018-05-22 18:43:35 +0530

    added router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { environment } from './../environments/environment';
 
+const routes: Routes = [
+  // { path: '', redirectTo: '/user-login', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +18,8 @@ import { environment } from './../environments/environment';
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
