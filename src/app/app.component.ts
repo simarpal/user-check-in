6e89857037aa54135c9a8f1508b022913fee3d21
simarpal <simerpal.sangha@gmail.com>
2018-05-24 17:58:30 +0530

    logout and already logedin navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,27 @@
 import { AngularFireAuth } from 'angularfire2/auth';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor(private afAuth: AngularFireAuth) {
-    console.log(afAuth);
-   }
-
+  constructor(private afAuth: AngularFireAuth,
+    private router: Router) {
+  }
+  
+  async ngOnInit() {
+    let loginUser = await this.afAuth.user;
+    if (loginUser) {
+      this.router.navigate(['/check-in']);
+    }
+  }
 
   logout() {
     this.afAuth.auth.signOut();
+    this.router.navigate(['/login']);    
   }
 }
